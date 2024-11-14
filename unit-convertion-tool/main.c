1f1c83d469d2dd08b633624577373cf794313115
rameem2003 <rameem2019@gmail.com>
2024-11-14 22:20:20 +0600

    update

--- unit-convertion-tool/main.c
+++ unit-convertion-tool/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <windows.h>
 
 void convertLength();
 void convertWeight();
@@ -6,7 +7,7 @@ void convertTemperature();
 
 void convertLength()
 {
-    int choice;
+    int ch;
     double value;
 
     printf("\n\033[1;36mYou Choose Length Conversion:\033[0m\n\n");
@@ -16,26 +17,54 @@ void convertLength()
     printf("\033[1;36m4. Centimeters to Inches\033[0m\n");
 
     printf("\033[1;32mEnter your choice (1-4): \033[0m\t");
-    scanf("%d", &choice);
+    scanf("%d", &ch);
     printf("\033[1;36mEnter the value to convert: \033[0m\t");
     scanf("%lf", &value);
 
-    switch (choice)
+    switch (ch)
     {
     case 1:
-        printf("%.2lf meters is %.2lf kilometers\n", value, value / 1000);
+        printf("\033[1;32mCalculating.........\033[0m");
+        Sleep(3000);
+        printf("\n\n\033[1;33m############################################################\033[0m\n");
+        printf("\033[1;33m###                                                   ###### \033[0m\n");
+        printf("\033[1;33m### %.2lf meters is %.2lf kilometers                  \033[0m\n", value, value / 1000);
+        printf("\033[1;33m###                                                   ###### \033[0m\n");
+        printf("\033[1;33m############################################################\033[0m\n\n\n\n");
         break;
     case 2:
-        printf("%.2lf kilometers is %.2lf meters\n", value, value * 1000);
+        printf("\033[1;32mCalculating.........\033[0m");
+        Sleep(3000);
+        printf("\n\n\033[1;33m############################################################\033[0m\n");
+        printf("\033[1;33m###                                                   ###### \033[0m\n");
+        printf("\033[1;33m### %.2lf kilometers is %.2lf meters                \033[0m\n", value, value * 1000);
+        printf("\033[1;33m###                                                   ###### \033[0m\n");
+        printf("\033[1;33m############################################################\033[0m\n\n\n\n");
         break;
     case 3:
-        printf("%.2lf inches is %.2lf centimeters\n", value, value * 2.54);
+        printf("\033[1;32mCalculating.........\033[0m");
+        Sleep(3000);
+        printf("\n\n\033[1;33m############################################################\033[0m\n");
+        printf("\033[1;33m###                                                   ###### \033[0m\n");
+        printf("\033[1;33m### %.2lf inches is %.2lf centimeters                  \033[0m\n", value, value * 2.54);
+        printf("\033[1;33m###                                                   ###### \033[0m\n");
+        printf("\033[1;33m############################################################\033[0m\n\n\n\n");
         break;
     case 4:
-        printf("%.2lf centimeters is %.2lf inches\n", value, value / 2.54);
+        printf("\033[1;32mCalculating.........\033[0m");
+        Sleep(3000);
+        printf("\n\n\033[1;33m############################################################\033[0m\n");
+        printf("\033[1;33m###                                                   ###### \033[0m\n");
+        printf("\033[1;33m### %.2lf centimeters is %.2lf inches              \033[0m\n", value, value / 2.54);
+        printf("\033[1;33m###                                                   ###### \033[0m\n");
+        printf("\033[1;33m############################################################\033[0m\n\n\n\n");
         break;
     default:
-        printf("Invalid choice.\n");
+        printf("\033[1;31m####################################################\033[0m\n");
+        printf("\033[1;31m#####                                          #####\033[0m\n");
+        printf("\033[1;31m#####    Invalid choice. Please try again.     #####\033[0m\n");
+        printf("\033[1;31m#####                                          #####\033[0m\n");
+        printf("\033[1;31m####################################################\033[0m\n\n\n");
     }
 }
 
@@ -44,33 +73,61 @@ void convertWeight()
     int choice;
     double value;
 
-    printf("\nWeight Conversion:\n");
-    printf("1. Grams to Kilograms\n");
-    printf("2. Kilograms to Grams\n");
-    printf("3. Pounds to Kilograms\n");
-    printf("4. Kilograms to Pounds\n");
+    printf("\n\n\033[1;35mYou Choose Weight Conversion:\033[0m\n");
+    printf("\n\033[1;35m1. Grams to Kilograms\033[0m\n");
+    printf("\n\033[1;35m2. Kilograms to Grams\033[0m\n");
+    printf("\n\033[1;35m3. Pounds to Kilograms\033[0m\n");
+    printf("\n\033[1;35m4. Kilograms to Pounds\033[0m\n");
 
-    printf("Enter your choice (1-4): ");
+    printf("\033[1;32mEnter your choice (1-4): \033[0m");
     scanf("%d", &choice);
-    printf("Enter the value to convert: ");
+    printf("\033[1;35mEnter the value to convert: \033[0m");
     scanf("%lf", &value);
 
     switch (choice)
     {
     case 1:
-        printf("%.2lf grams is %.2lf kilograms\n", value, value / 1000);
+        printf("\033[1;32mCalculating.........\033[0m");
+        Sleep(3000);
+        printf("\n\n\033[1;33m############################################################\033[0m\n");
+        printf("\033[1;33m###                                                   ###### \033[0m\n");
+        printf("\033[1;33m### %.2lf grams is %.2lf kilograms                 \033[0m\n", value, value / 1000);
+        printf("\033[1;33m###                                                   ###### \033[0m\n");
+        printf("\033[1;33m############################################################\033[0m\n\n\n\n");
         break;
     case 2:
-        printf("%.2lf kilograms is %.2lf grams\n", value, value * 1000);
+        printf("\033[1;32mCalculating.........\033[0m");
+        Sleep(3000);
+        printf("\n\n\033[1;33m############################################################\033[0m\n");
+        printf("\033[1;33m###                                                   ###### \033[0m\n");
+        printf("\033[1;33m### %.2lf kilograms is %.2lf grams                 \033[0m\n", value, value * 1000);
+        printf("\033[1;33m###                                                   ###### \033[0m\n");
+        printf("\033[1;33m############################################################\033[0m\n\n\n\n");
         break;
     case 3:
-        printf("%.2lf pounds is %.2lf kilograms\n", value, value * 0.453592);
+        printf("\033[1;32mCalculating.........\033[0m");
+        Sleep(3000);
+        printf("\n\n\033[1;33m############################################################\033[0m\n");
+        printf("\033[1;33m###                                                   ###### \033[0m\n");
+        printf("\033[1;33m### %.2lf pounds is %.2lf kilograms                  \033[0m\n", value, value * 0.453592);
+        printf("\033[1;33m###                                                   ###### \033[0m\n");
+        printf("\033[1;33m############################################################\033[0m\n\n\n\n");
         break;
     case 4:
-        printf("%.2lf kilograms is %.2lf pounds\n", value, value / 0.453592);
+        printf("\033[1;32mCalculating.........\033[0m");
+        Sleep(3000);
+        printf("\n\n\033[1;33m############################################################\033[0m\n");
+        printf("\033[1;33m###                                                   ###### \033[0m\n");
+        printf("\033[1;33m### %.2lf kilograms is %.2lf pounds                  \033[0m\n", value, value / 0.453592);
+        printf("\033[1;33m###                                                   ###### \033[0m\n");
+        printf("\033[1;33m############################################################\033[0m\n\n\n\n");
         break;
     default:
-        printf("Invalid choice.\n");
+        printf("\033[1;31m####################################################\033[0m\n");
+        printf("\033[1;31m#####                                          #####\033[0m\n");
+        printf("\033[1;31m#####    Invalid choice. Please try again.     #####\033[0m\n");
+        printf("\033[1;31m#####                                          #####\033[0m\n");
+        printf("\033[1;31m####################################################\033[0m\n\n\n");
     }
 }
 
--- unit-convertion-tool/tempCodeRunnerFile.c
+++ unit-convertion-tool/tempCodeRunnerFile.c
@@ -1,2 +1,1 @@
-
-        printf("\033[1;31mExiting...\033[0m\n\n
+%.2lf kilometers is %.2lf meters          
